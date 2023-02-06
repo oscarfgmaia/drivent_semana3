4f5dfd9798d4960399db76d180ee9326a09ca59d
oscarfgmaia <oscarfgmaia@gmail.com>
2023-02-06 16:10:34 -0300

    refact: send userId to get hotels

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -4,12 +4,16 @@ import { Response } from 'express';
 import httpStatus from 'http-status';
 
 export async function getHotels(req: AuthenticatedRequest, res: Response) {
+  const userId = req.userId;
+
   try {
-    const hotels = await hotelService.getHotels();
+    const hotels = await hotelService.getHotels(userId);
     res.send(hotels).status(200);
   } catch (error) {
     if (error.name === 'NotFoundError') {
       return res.sendStatus(httpStatus.NOT_FOUND);
+    } else if (error.name === 'PaymentError') {
+      return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
     } else {
       return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
     }
