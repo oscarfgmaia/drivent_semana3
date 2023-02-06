7d2ac60399fd661d2ab19c23c1859f07a4746907
oscarfgmaia <oscarfgmaia@gmail.com>
2023-02-06 16:10:48 -0300

    feat: create payment handling on middleware

--- src/middlewares/error-handling-middleware.ts
+++ src/middlewares/error-handling-middleware.ts
@@ -33,6 +33,12 @@ export function handleApplicationErrors(err: ApplicationError | Error, _req: Req
     });
   }
 
+  if (err.name === "PaymentError") {
+    return res.status(httpStatus.PAYMENT_REQUIRED).send({
+      message: err.message,
+    });
+  }
+
   /* eslint-disable-next-line no-console */
   console.error(err.name);
   res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
